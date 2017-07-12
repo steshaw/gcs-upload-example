4665f4ed2b51bf620caed58f89a5c00010444a85
Steven Shaw <steven@steshaw.org>
2017-07-12 18:12:02 +1000

    Initial

---
+++ src/Main.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import Control.Lens ((&), (.~), (<&>), (?~))
+import Data.Text (Text)
+import Network.Google
+import Network.Google.Storage
+import System.IO (stdout)
+import System.Environment as Env
+
+import qualified Data.Text as T
+
+uploadIt :: Text -> FilePath -> IO Object
+uploadIt bucket filename = do
+  lgr <- newLogger Debug stdout -- (1)
+  env <- newEnv <&> (envLogger .~ lgr) . (envScopes .~ storageReadWriteScope) -- (2) (3)
+  body <- sourceBody filename -- (4)
+  let key = T.pack filename
+      bkt = bucket
+  runResourceT . runGoogle env $ -- (5)
+    upload (objectsInsert bkt object' & oiName ?~ key) body
+
+main :: IO ()
+main = do
+  args <- Env.getArgs
+  case args of
+    [ bucket, filename ] -> do
+      putStrLn "Uploading..."
+      object <- uploadIt (T.pack bucket) filename
+      print object
