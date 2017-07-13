8d0acf9beaadaf3484a159f667e3895f757d04b1
Steven Shaw <steven@steshaw.org>
2017-07-13 10:00:53 +1000

    Prettily output the response object

--- src/Main.hs
+++ src/Main.hs
@@ -8,16 +8,23 @@ import Network.Google
 import Network.Google.Storage
 import System.IO (stdout)
 
+import Language.Haskell.HsColour
+import Language.Haskell.HsColour.Colourise
+import Text.Show.Pretty (ppShow)
+
 import qualified Data.Text as T
 import qualified System.Environment as Env
 
+pprint :: Show a => a -> IO ()
+pprint =
+  putStrLn . hscolour TTY defaultColourPrefs False False "" False . ppShow
+
 uploadIt :: Text -> FilePath -> IO Object
-uploadIt bucket filename = do
+uploadIt bkt filename = do
   lgr <- newLogger Debug stdout -- (1)
   env <- newEnv <&> (envLogger .~ lgr) . (envScopes .~ storageReadWriteScope) -- (2) (3)
   body <- sourceBody filename -- (4)
   let key = T.pack filename
-      bkt = bucket
   runResourceT . runGoogle env $ -- (5)
     upload (objectsInsert bkt object' & oiName ?~ key) body
 
@@ -25,7 +32,8 @@ main :: IO ()
 main = do
   args <- Env.getArgs
   case args of
-    [bucket, filename] -> do
+    [bkt, filename] -> do
       putStrLn "Uploading..."
-      object <- uploadIt (T.pack bucket) filename
-      print ("the object", object)
+      object <- uploadIt (T.pack bkt) filename
+      pprint ("the object" :: String, object)
+    _ -> putStrLn "usage: gcs-upload bucket-name filename"
